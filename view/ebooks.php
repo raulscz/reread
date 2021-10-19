2f02e137437d1250e7bd87fb7365385b5449fdf4
Raul <100007530.joan23@fje.edu>
2021-10-19 16:08:23 +0200

    Actividad 4 Terminada

--- view/ebooks.php
+++ view/ebooks.php
@@ -28,33 +28,43 @@
                 <a href="libros.php">Libros</a>
                 <a href="ebooks.php" class="active">eBooks</a>
             </div>
+            
             <div class="textpage">
                 <h3>Toda la actualidad en eBook</h3>
-                <div class="gallery">
-                    <img src="../img/cell.jpeg" alt="Cell">
-                    <div class="desc">A través de los teléfonos móviles se envía un mensaje que convierte a todos en esclavos asesinos...</div>
-                </div>
+                <?php
+                        require_once '../services/connection.php';
 
+                        $sql= "SELECT books.Title, books.Description, books.img FROM `books` WHERE books.eBook = 1";
+                        $result = mysqli_query($conn,$sql);
+
+                        foreach ($result as $registro){             
+                    ?>
                 <div class="gallery">
-                    <img src="../img/elciclodelhombrelobo.jpeg" alt="El ciclo del hombre lobo">
+                    <img src="../img/<?php echo"$registro[img]";?>">
+                    <div><?php echo"$registro[Title]";?></div>
+                    <div><?php echo"$registro[Description]";?></div>
+                </div>
+                <?php } ?>
+                <!-- <div class="gallery">
+                    <img src="" alt="El ciclo del hombre lobo">
                     <div class="desc">Una escalofriante revisión del mito del hombre lobo por el rey de la literatura de terror...</div>
                 </div>
 
                 <div class="gallery">
-                    <img src="../img/elresplandor.jpeg" alt="El resplandor">
+                    <img src="" alt="El resplandor">
                     <div class="desc">Esa es la palabra que Danny había visto en el espejo. Y, aunque no sabía leer, entendió que era un mensaje de horror...</div>
                 </div>
 
                 <div class="gallery clear">
-                    <img src="../img/doctorsleep.jpeg" alt="doctorsleep">
+                    <img src="" alt="doctorsleep">
                     <div class="desc">Una novela que entusiasmará a los millones de lectores de El resplandor y que encantará...</div>
                 </div>
 
                 <div class="gallery">
-                    <img src="../img/mientrasescribo.jpeg" alt="Mientras escribo">
+                    <img src="" alt="Mientras escribo">
                     <div class="desc">Pocas veces un libro sobre el oficio de escribir ha resultado tan clarificador, útil y revelador.</div>
-                </div>
-
+                </div> -->
+                
             </div>
         </div>
         <div class="column side">
